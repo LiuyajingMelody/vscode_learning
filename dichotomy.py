bc56b1bce2004e2105cc63677cbbb9d21bf82e37
LiuyajingMelody <1440614396@qq.com>
2019-03-05 21:46:06 +0800

    new change

--- dichotomy.py
+++ dichotomy.py
@@ -1,9 +1,12 @@
+
+
+import math
 def binary(list, item):
     low = 0
     high = len(list)-1
     while low <= high:
-        mid = ((low+high)/2)
-        guess = list(mid)
+        mid = math.floor((low+high)/2)
+        guess = list[mid]
         if guess == item:
             return mid
         if guess <= item:
@@ -11,3 +14,7 @@ def binary(list, item):
         else:
             high = mid+1
         return None
+
+
+my_listing = [1, 2, 3, 4, 5, 6, 7]
+print(binary(my_listing, 4))
